972765856ea69697b1c3c868440ac62153ec6abe
Julien Ripouteau <julien@ripouteau.com>
2026-03-23 15:46:40 +0100

    feat: add otel export path

---
+++ src/otel.ts
@@ -0,0 +1,11 @@
+/*
+ * @adonisjs/queue
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { QueueInstrumentation } from '@boringnode/queue/otel'
+export type { QueueInstrumentationConfig } from '@boringnode/queue/otel'
